3fa54c134365b390128f776a509115770914faff
artoxaaa <artoxaaa5@gmail.com>
2019-11-24 14:47:34 +0200

    second commit

--- Puzzle.cs
+++ Puzzle.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Threading;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace _15puzzleWPF
 {
